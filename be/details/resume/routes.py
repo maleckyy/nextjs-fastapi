7f035bee977f4d87a5b46ff823f448144f0777b5
maleckyy <kupferpatryk@gmail.com>
2025-07-26 18:39:49 +0200

    resume-stack

--- be/details/resume/routes.py
+++ be/details/resume/routes.py
@@ -1,5 +1,4 @@
-import datetime
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, HTTPException, Response
 from jinja2 import Environment, FileSystemLoader
 from fastapi import APIRouter, Depends
 from auth.routes import get_current_user
@@ -16,8 +15,26 @@ router = APIRouter(
 
 @router.get("")
 def get_resume_html(db: db_dependency, current_user: models.Users = Depends(get_current_user)):
+
+    profile_details = db.query(models.UserDetails).filter(models.UserDetails.user_id == current_user.id).first()
+    
+    if not profile_details:
+        raise HTTPException(status_code=404, detail="User profile not found")
+
+    profile_stack = db.query(models.UserProfileStack).filter(models.UserProfileStack.user_id == current_user.id).first()
+    formatted_stack = profile_stack.stack.replace(",", ", ")
+
+
+    print(profile_details)
+
+
     template = env.get_template("resume.html")
     html = template.render(
-        username=current_user.email
+        first_name=profile_details.first_name,
+        last_name=profile_details.last_name,
+        description=profile_details.description,
+        profile_stack=formatted_stack,
+        email= current_user.email,
+        phone_number= profile_details.phone_number,
     )
     return Response(content=html, media_type="text/html")
