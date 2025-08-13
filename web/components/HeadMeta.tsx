5baf8e9b88088b14d1f15f8eb101ac3f1fa8fdd3
maleckyy <kupferpatryk@gmail.com>
2025-08-13 15:58:36 +0200

    head-meta-update

--- web/components/HeadMeta.tsx
+++ web/components/HeadMeta.tsx
@@ -1,3 +1,4 @@
+import { appDescription, appName } from '@/env/STATIC_NAMES'
 import Head from 'next/head'
 import React from 'react'
 
@@ -9,11 +10,11 @@ export default function HeadMeta() {
         name='viewport'
         content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
       />
-      <meta name="application-name" content="PWA App" />
+      <meta name="application-name" content={appName} />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-mobile-web-app-title" content="PWA App" />
-      <meta name="description" content="Best PWA App in the world" />
+      <meta name="apple-mobile-web-app-title" content={appName} />
+      <meta name="description" content={appDescription} />
       <meta name="mobile-web-app-capable" content="yes" />
     </Head>
   )
